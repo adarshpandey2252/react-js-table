adc61563004297975f0339805faea1ae0843c227
adarshpandey2252 <102797876+adarshpandey2252@users.noreply.github.com>
2023-04-16 14:55:47 +0530

    react js table

---
+++ src/Table.jsx
@@ -0,0 +1,172 @@
+import React from 'react'
+
+const Table = () => {
+  return (
+    <>
+     <div className="container">
+
+<table >
+ <tr className="first">
+<th >MONTH1</th>
+<th></th>
+<th></th>
+<th></th>
+<th></th>
+<th ></th>
+<th></th>
+
+ </tr>
+  <tr>
+    <th className="sm">Onboarding Call</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <th className="sm"> Google Search Console Access
+    </th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <th className="sm">Google Analytics Access</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <th className="sm">Website Access</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <th className="sm">Technical Audit</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <th className="sm">Anchor Text / URL Mapping</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <th className="sm">Google Data Studio Report + Local Reporting Suite</th>
+    <th></th>
+    <th></th><th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+
+  </tr>
+  <tr>
+    <th className="sm">Site Level Optimization</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <th className="sm">On Page Oprtimization </th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <th className="sm">Content Creation</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <th className="sm">Content Publishing</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <th className="sm">Premium Press Release</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <th className="sm">Authority Niche Placements</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <th className="sm">Review Management</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <th className="sm">Index Links</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <th className="sm">Video Recap</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+</table>
+
+</div>
+
+    </>
+  )
+}
+
+export default Table
